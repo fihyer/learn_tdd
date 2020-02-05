828764fbabc1acf0ac56e9077f5d0d225767f435
fihyer <fihyer@me.com>
2020-02-05 17:39:50 +0800

    refactoring functional test with unit test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,8 +1,48 @@
 # -*- coding: utf-8 -*- 
-from selenium.webdriver import Chrome
+#  from selenium.webdriver import Chrome
+from selenium import webdriver
+import unittest
 
-browser = Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
 
 
-assert 'Django' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Story Begains
+
+        # Edith has heard about a cool new online to-do app
+        # She goes to check out its homepage
+        browser.get('http://localhost:8000')
+
+
+        #  assert 'Django' in browser.title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types " Buy peacock feathers" into a text box
+
+        # When she hits enter, the page updates, and now the apge lists
+        # "1: By peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list.
+        # Then she sees that the site has generated a unique URL for her
+        # there is some explanatory text to that effect.
+
+        # She vistis that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
