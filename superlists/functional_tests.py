b90220e0c0f20574c103ec77ff32bb2065eea989
fihyer <fihyer@me.com>
2020-02-07 22:39:47 +0800

    Saving user input data

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -41,20 +42,29 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the apge lists
         # "1: By peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(2)
+        self.browser.implicitly_wait(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        slef.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        #  self.assertTrue(
+            #  any(row.text == '1: Buy peacock feathers' for row in rows),
+            #  f"New to-do item did not appear in table. Contents were:\n{table.text}"
+        #  )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
+        input_box = self.browser.find_element_by_id('id_new_item')
+
         # She enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        self.browser.implicitly_wait(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique URL for her
@@ -63,6 +73,7 @@ class NewVisitorTest(unittest.TestCase):
         # She vistis that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
